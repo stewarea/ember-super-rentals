66d37c2455540dfd7346dc3dfbfcfb1d4dd95c3c
Evan <stewarea@gmail.com>
2016-10-24 12:59:56 -0700

    initial commit

--- config/environment.js
+++ config/environment.js
@@ -4,22 +4,24 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'super-rentals',
     environment: environment,
-    rootURL: '/',
+    baseURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
       }
     },
 
+    firebase: {
+      apiKey: 'AIzaSyCAUQPUY5lYM2ZQcFhJLESKi2BCMIED1EM',
+      authDomain: 'super-rentals-33308.firebaseapp.com',
+      databaseURL: 'https://super-rentals-33308.firebaseio.com',
+      storageBucket: 'super-rentals-33308.appspot.com'
+    },
+
     APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+
     }
   };
 
