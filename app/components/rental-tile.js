864e65267066f9afe8b2258568a3255a77a26af8
Evan <stewarea@gmail.com>
2016-10-24 20:54:50 -0700

    add update function

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,13 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isImageShowing: false
+  isImageShowing: false,
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
     },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+
+    update(rental, params) {
+      this.sendAction('update', rental, params);
+    },
+    delete(rental) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
     }
   }
 });
