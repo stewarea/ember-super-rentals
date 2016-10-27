cc46a2772b71c4e877ba97c95bb0209af6f729ac
Evan Stewart and Martin Cartledge <martin@>
2016-10-27 09:18:28 -0700

    remove reviews from index and create review tile which adds reviews

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,19 +7,12 @@ export default Ember.Route.extend({
       reviews: this.store.findAll('review')
   });
 },
-
   actions: {
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
       this.transitionTo('index');
     },
-    saveReview(params) {
-      var newReview = this.store.createRecord('review', params);
-      newReview.save();
-      this.transitionTo('index');
-    },
-
     update(rental, params) {
       Object.keys(params).forEach(function(key) {
     if(params[key]!==undefined) {
